f2d324c2eab9b7946d64573b519be6dac02ee1a6
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-05-15 18:22:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//=================Assignment 3=============================//
+//================VARIABLES FOR STRINGS===================//
+
+
+
+//Q1:Declare a variable called age and assign to it your age. Show your age in an alert box.
+//Ans:
+// var age = 17;
+// alert ( "I am " + age + " years old"  );
+
+
+//Q2:Declare and initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "you have visited this site N times."
+//Ans:
+
+
+//Q3:Declare a variable called birthYear and assign to it your birthYear. 
+//Ans:
+// var birthYear = 2006;
+// document.write ("My birth year is " + birthYear );
+// document.write ("<br>");
+// document.write ("Data type of my declared variable is number.");
+
+
+//Q4:A visitor visits an online clothing store ww.xyzClothing.com.
+//   Write a script to store in variables the following information.
+//   a.Visitors's name
+//   b.Product title
+//   c.Quantity i.e how many products a visitor wants to order
+//   Show the following message in your browser: "Jhon Doe ordered 5 T-shirts(s) on XYZ Clothing Store."
+//Ans:
+// let VisitorsName = prompt("What is your name?");
+// let ProductTitle = prompt( "Write the product name you want to buy.");
+// let numberOfOrders = +prompt( "How many " + ProductTitle + " you want to buy");
+// document.write ( VisitorsName +  " ordered " + numberOfOrders + ProductTitle + " on XYZ Clothing store.");
+
+
